38684511f3e91c977bbd2a9f48f22edf119b204c
Nycto-c05 <creflo.c05@gmail.com>
2025-11-29 18:27:02 +0530

    update

---
+++ go/03-channels/channel/main2.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+var wg sync.WaitGroup
+
+func one() {
+	// wont work, unbuff need a receiving routine
+	c := make(chan int)
+	c <- 1
+	i := <-c
+	fmt.Println(i)
+}
+
+func two() {
+	var wg sync.WaitGroup
+	//here, routine sends, and main rotine receives
+	// when pushin from main routine and recieve in another routine
+	// the recieving routine must be ready, so c<-2 is below
+	//else deadlock
+
+	//had to add wg, cuz exits before printing
+	c := make(chan int)
+
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		val := <-c // waits and listens for val
+		fmt.Println(val)
+	}()
+
+	time.Sleep(2 * time.Second)
+	c <- 2
+	wg.Wait()
+}
+
+func three() {
+	//main routine recieves n go routine sends
+	c := make(chan string)
+
+	// wg.Add(1)
+	go func(s string) {
+		// defer wg.Done()
+		//This below will block until main goroutine is ready to receive.
+		//if all other routines are asleep/blocked, then deadlock
+		c <- s
+	}("ligma")
+	// wg.Wait()//blocks main
+
+	time.Sleep(2 * time.Second) //even tho main is blocked, there is a possible path, hence no deadlock
+	fmt.Println(<-c)
+
+}
+
+func main() {
+	// one()
+	// two()
+	three()
+}
