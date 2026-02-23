865dceea5847bed14018d3c7e75b25768c862b5d
Creflo Coelho <creflo.c05@gmail.com>
2026-02-23 21:39:14 +0530

    Update k8s

---
+++ go/grpc/proto_exa/go.mod
@@ -0,0 +1,3 @@
+module github.com/Nycto-c05/proto_exa
+
+go 1.25.5
--- minio-go/internal/storage/storage.go
+++
@@ -1,9 +0,0 @@
-package storage
-
-import "context"
-
-type ObjectStore interface {
-	Get(ctx context.Context, key string) ([]byte, error)
-	Put(ctx context.Context, key string, data []byte) error
-	Delete(ctx context.Context, key string) error
-}
